8a096445d81c8db705563815865ee1e7ea2f39e5
Masayuki Takagi <kamonama@gmail.com>
2014-07-05 00:51:41 +0900

    modify to prepare project

---
+++ src/cl-voxelize.lisp
@@ -0,0 +1,11 @@
+#|
+  This file is a part of cl-voxelize project.
+  Copyright (c) 2014 Masayuki Takagi (kamonama@gmail.com)
+|#
+
+(in-package :cl-user)
+(defpackage cl-voxelize
+  (:use :cl))
+(in-package :cl-voxelize)
+
+;; blah blah blah.
---
+++ t/cl-voxelize.lisp
@@ -0,0 +1,17 @@
+#|
+  This file is a part of cl-voxelize project.
+  Copyright (c) 2014 Masayuki Takagi (kamonama@gmail.com)
+|#
+
+(in-package :cl-user)
+(defpackage cl-voxelize-test
+  (:use :cl
+        :cl-voxelize
+        :cl-test-more))
+(in-package :cl-voxelize-test)
+
+(plan nil)
+
+;; blah blah blah.
+
+(finalize)
